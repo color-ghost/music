2dd06ab146f075732b699584dfaf9cf7d4bfedc6
color-ghost <53136198+color-ghost@users.noreply.github.com>
2020-03-27 13:14:11 -0700

    Update tracks.js

--- js/data/tracks.js
+++ js/data/tracks.js
@@ -2,7 +2,7 @@ export const trackList = [
   {
     url: 'https://www.douglaskdlc.com//magic-of-the-mind.wav',
     art: 'images/fiji.png',
-    time: '37:44',
+    time: '37:39',
     title: 'Magic of the Mind',
     artist: 'color ghost',
   }
