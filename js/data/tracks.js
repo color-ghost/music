ffdaa119ac42331eb1fafd3a3a839a614a4e4054
color-ghost <53136198+color-ghost@users.noreply.github.com>
2020-03-27 13:09:52 -0700

    Add files via upload

---
+++ js/data/tracks.js
@@ -0,0 +1,9 @@
+export const trackList = [
+  {
+    url: 'https://www.douglaskdlc.com//magic-of-the-mind.wav',
+    art: 'images/fiji.png',
+    time: '37:44',
+    title: 'Magic of the Mind',
+    artist: 'color ghost',
+  }
+];
