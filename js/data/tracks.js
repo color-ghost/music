f64cefdc77f3ccf8f1c45bf6e266a7af1a139b66
color-ghost <53136198+color-ghost@users.noreply.github.com>
2020-04-01 20:03:32 -0700

    Update tracks.js

--- js/data/tracks.js
+++ js/data/tracks.js
@@ -1,6 +1,13 @@
 export const trackList = [
   {
-    url: 'https://www.douglaskdlc.com//magic-of-the-mind.wav',
+    url: 'https://www.douglaskdlc.com/one-in-a-million-we-are-twice.wav',
+    art: 'images/twice.png',
+    time: '1:07:49',
+    title: 'One In A Million We Are TWICE',
+    artist: 'color ghost',
+  },
+  {
+    url: 'https://www.douglaskdlc.com/magic-of-the-mind.wav',
     art: 'images/fiji.png',
     time: '37:39',
     title: 'Magic of the Mind',
